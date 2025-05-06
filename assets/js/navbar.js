febef92e5cbfc3a3a1c3235c779afb118aacde99
alexistb2904 <alexistb2904@gmail.com>
2025-05-06 11:35:38 +0200

    Changement, fiches, navbar, tableau de synthese

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,65 +1,151 @@
-const nav_mobile_E4 = document.querySelector("#mobile-bar #epreuveE5");
-const nav_mobile_E5 = document.querySelector("#mobile-bar #epreuveE6");
-const nav_mobile_veille = document.querySelector("#mobile-bar #veille");
-
-const mobile_E4 = document.querySelector("#mobile-bar #epreuveE5 #mobile-E5");
-const mobile_E5 = document.querySelector("#mobile-bar #epreuveE6 #mobile-E6");
-const mobile_veille = document.querySelector("#mobile-bar #veille #mobile-veille");
-
-function AppearMobile(name) {
-  if (name.style.display == "none") {
-    name.style.display = "block";
-  } else {
-    name.style.display = "none";
+document.addEventListener("DOMContentLoaded", () => {
+  function buildPath(relativePath) {
+    const currentPath = window.location.origin + (relativePath != "" ? "/" + relativePath : "");
+    return currentPath;
   }
-}
 
-function DisAppearMobile(name) {
-  if (name.style.display == "block") {
-    name.style.display = "none";
+  const navHTML = `
+    <nav class="mobile-active" id="generated-nav">
+      <span id="computer-bar">
+        <a href="${buildPath("")}"><img src="${buildPath("assets/img/logo_white.png")}" alt="Logo" /></a>
+        <ul id="nav-bar">
+          <li><a href="${buildPath("#presentation")}">Présentation</a></li>
+          <li id="epreuveE5">
+            <a href="${buildPath("epreuveE5/")}">Epreuve E5</a>
+            <div id="epreuveE5-dropdown">
+              <ul>
+                <li><a href="${buildPath("epreuveE5/#synthese")}">Tableau de synthèse</a></li>
+                <li><a href="${buildPath("epreuveE5/#formation")}">Missions réalisées en formation</a></li>
+                <li><a href="${buildPath("epreuveE5/#entreprise")}">Missions réalisées en Entreprise</a></li>
+              </ul>
+            </div>
+          </li>
+          <li id="epreuveE6">
+            <a href="${buildPath("epreuveE6/")}">Epreuve E6</a>
+            <div id="epreuveE6-dropdown">
+              <ul>
+                <li><a href="${buildPath("epreuveE6/#PPEDESKTOP")}">Situation 1</a></li>
+                <li><a href="${buildPath("epreuveE6/#PPEWEB")}">Situation 2</a></li>
+                <li><a href="${buildPath("epreuveE6/certification")}">Certifications</a></li>
+              </ul>
+            </div>
+          </li>
+          <li id="veille">
+            <a href="${buildPath("veille/")}">Veille Technologique</a>
+            <div id="veille-dropdown">
+              <ul>
+                <li><a href="${buildPath("veille/flux/")}">Flux</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+        <i class="ai-toggle-off-fill" id="theme-button"></i>
+      </span>
+      <span id="mobile-bar">
+        <ul id="nav-bar">
+          <li><a href="${buildPath("#presentation")}">Présentation</a></li>
+          <li id="epreuveE5" class="epreuveAnimation">
+            <a>Epreuve E5</a>
+            <div id="mobile-E5">
+              <ul>
+                <li><a href="${buildPath("epreuveE5/#synthese")}">Tableau de synthèse</a></li>
+                <li><a href="${buildPath("epreuveE5/#formation")}">Missions réalisées en formation</a></li>
+                <li><a href="${buildPath("epreuveE5/#entreprise")}">Missions réalisées en Entreprise</a></li>
+              </ul>
+            </div>
+          </li>
+          <li id="epreuveE6" class="epreuveAnimation">
+            <a>Epreuve E6</a>
+            <div id="mobile-E6">
+              <ul>
+                <li><a href="${buildPath("epreuveE6/#PPEDESKTOP")}">Situation 1</a></li>
+                <li><a href="${buildPath("epreuveE6/#PPEWEB")}">Situation 2</a></li>
+                <li><a href="${buildPath("epreuveE6/certification")}">Certifications</a></li>
+              </ul>
+            </div>
+          </li>
+          <li id="veille" class="epreuveAnimation">
+            <a>Veille Technologique</a>
+            <div id="mobile-veille">
+              <ul>
+                <li><a href="${buildPath("veille/")}">Veille</a></li>
+                <li><a href="${buildPath("veille/flux/")}">Flux</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+        <i class="ai-toggle-off-fill" id="theme-button-mobile"></i>
+        <a href="${buildPath("")}"><img src="${buildPath("assets/img/logo_white.png")}" alt="Logo" /></a>
+        <hr />
+      </span>
+    </nav>
+  `;
+  const navContainer = document.querySelector("header");
+  navContainer.innerHTML = navHTML;
+
+  const nav_mobile_E4 = document.querySelector("#mobile-bar #epreuveE5");
+  const nav_mobile_E5 = document.querySelector("#mobile-bar #epreuveE6");
+  const nav_mobile_veille = document.querySelector("#mobile-bar #veille");
+
+  const mobile_E4 = document.querySelector("#mobile-bar #epreuveE5 #mobile-E5");
+  const mobile_E5 = document.querySelector("#mobile-bar #epreuveE6 #mobile-E6");
+  const mobile_veille = document.querySelector("#mobile-bar #veille #mobile-veille");
+
+  function AppearMobile(name) {
+    if (name.style.display == "none") {
+      name.style.display = "block";
+    } else {
+      name.style.display = "none";
+    }
   }
-}
 
-nav_mobile_E4.addEventListener("click", function () {
-  AppearMobile(mobile_E4);
-  DisAppearMobile(mobile_E5);
-  DisAppearMobile(mobile_veille);
+  function DisAppearMobile(name) {
+    if (name.style.display == "block") {
+      name.style.display = "none";
+    }
+  }
+
+  nav_mobile_E4.addEventListener("click", function () {
+    AppearMobile(mobile_E4);
+    DisAppearMobile(mobile_E5);
+    DisAppearMobile(mobile_veille);
+  });
+
+  nav_mobile_E5.addEventListener("click", function () {
+    AppearMobile(mobile_E5);
+    DisAppearMobile(mobile_E4);
+    DisAppearMobile(mobile_veille);
+  });
+
+  nav_mobile_veille.addEventListener("click", function () {
+    AppearMobile(mobile_veille);
+    DisAppearMobile(mobile_E4);
+    DisAppearMobile(mobile_E5);
+  });
+
+  window.onload = function () {
+    mobile_E4.style.display = "none";
+    mobile_E5.style.display = "none";
+    mobile_veille.style.display = "none";
+  };
+
+  function isOnline() {
+    let isTabVisible = true;
+    if (typeof document.hidden !== "undefined") {
+      isTabVisible = !document.hidden;
+    } else if (typeof document.msHidden !== "undefined") {
+      isTabVisible = !document.msHidden;
+    } else if (typeof document.webkitHidden !== "undefined") {
+      isTabVisible = !document.webkitHidden;
+    }
+
+    if (isTabVisible) {
+      document.title = "Alexis Thierry-Bellefond";
+    } else {
+      document.title = "C'est moi Alexis !";
+    }
+    setTimeout(isOnline, 150);
+  }
+
+  isOnline();
 });
-
-nav_mobile_E5.addEventListener("click", function () {
-  AppearMobile(mobile_E5);
-  DisAppearMobile(mobile_E4);
-  DisAppearMobile(mobile_veille);
-});
-
-nav_mobile_veille.addEventListener("click", function () {
-  AppearMobile(mobile_veille);
-  DisAppearMobile(mobile_E4);
-  DisAppearMobile(mobile_E5);
-});
-
-window.onload = function () {
-  mobile_E4.style.display = "none";
-  mobile_E5.style.display = "none";
-  mobile_veille.style.display = "none";
-};
-
-function isOnline() {
-  let isTabVisible = true;
-  if (typeof document.hidden !== "undefined") {
-    isTabVisible = !document.hidden;
-  } else if (typeof document.msHidden !== "undefined") {
-    isTabVisible = !document.msHidden;
-  } else if (typeof document.webkitHidden !== "undefined") {
-    isTabVisible = !document.webkitHidden;
-  }
-
-  if (isTabVisible) {
-    document.title = "Alexis Thierry-Bellefond";
-  } else {
-    document.title = "C'est moi Alexis !";
-  }
-  setTimeout(isOnline, 150);
-}
-
-isOnline();
--- assets/js/style.js
+++ assets/js/style.js
@@ -1,89 +1,98 @@
-const darkModeToggle = document.querySelector("#theme-button");
-const darkModeToggleMobile = document.querySelector("#theme-button-mobile");
-const Paris = document.querySelector("#villedeparis img");
-function activateDarkMode() {
-  darkModeToggle.classList.remove("ai-toggle-off-fill");
-  darkModeToggleMobile.classList.remove("ai-toggle-off-fill");
-  darkModeToggle.classList.add("ai-toggle-on-fill");
-  darkModeToggle.classList.add("checked");
-  darkModeToggleMobile.classList.add("ai-toggle-on-fill");
-  darkModeToggleMobile.classList.add("checked");
-  document.body.classList.remove("light");
-  document.body.classList.add("dark");
-  if (Paris) {
-    Paris.style.filter = "";
+document.addEventListener("DOMContentLoaded", () => {
+  let darkModeToggle = document.querySelector("#theme-button");
+  let darkModeToggleMobile = document.querySelector("#theme-button-mobile");
+
+  while (!darkModeToggle || !darkModeToggleMobile) {
+    console.log("Waiting for dark mode toggle elements to load...");
+    darkModeToggle = document.querySelector("#theme-button");
+    darkModeToggleMobile = document.querySelector("#theme-button-mobile");
   }
-}
-
-function activateLightMode() {
-  darkModeToggle.classList.remove("ai-toggle-on-fill");
-  darkModeToggle.classList.remove("checked");
-  darkModeToggleMobile.classList.remove("ai-toggle-on-fill");
-  darkModeToggleMobile.classList.remove("checked");
-  darkModeToggle.classList.add("ai-toggle-off-fill");
-  darkModeToggleMobile.classList.add("ai-toggle-off-fill");
-  document.body.classList.remove("dark");
-  document.body.classList.add("light");
-  if (Paris) {
-    Paris.style.filter = "brightness(0)";
-  }
-}
-
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+  console.log("Dark mode toggle elements loaded successfully.");
+  const Paris = document.querySelector("#villedeparis img");
+  function activateDarkMode() {
+    darkModeToggle.classList.remove("ai-toggle-off-fill");
+    darkModeToggleMobile.classList.remove("ai-toggle-off-fill");
+    darkModeToggle.classList.add("ai-toggle-on-fill");
+    darkModeToggle.classList.add("checked");
+    darkModeToggleMobile.classList.add("ai-toggle-on-fill");
+    darkModeToggleMobile.classList.add("checked");
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    if (Paris) {
+      Paris.style.filter = "";
     }
   }
-  return "";
-}
 
-const lightModePreference = window.matchMedia("(prefers-color-scheme: light)");
-lightModePreference.addEventListener("change", (e) => e.matches && activateLightMode());
-lightModePreference.addListener((e) => e.matches && activateLightMode());
-if (lightModePreference.matches) {
-  if (getCookie("theme") == "light") {
-    activateLightMode();
-  } else {
-    activateDarkMode();
+  function activateLightMode() {
+    darkModeToggle.classList.remove("ai-toggle-on-fill");
+    darkModeToggle.classList.remove("checked");
+    darkModeToggleMobile.classList.remove("ai-toggle-on-fill");
+    darkModeToggleMobile.classList.remove("checked");
+    darkModeToggle.classList.add("ai-toggle-off-fill");
+    darkModeToggleMobile.classList.add("ai-toggle-off-fill");
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    if (Paris) {
+      Paris.style.filter = "brightness(0)";
+    }
   }
-} else {
-  if (getCookie("theme") == "dark") {
-    activateDarkMode();
-  } else {
-    activateLightMode();
-  }
-}
 
-darkModeToggle.addEventListener("click", () => {
-  if (document.body.classList.contains("light")) {
-    activateDarkMode();
-    setCookie("theme", "dark", 365);
-  } else {
-    activateLightMode();
-    setCookie("theme", "light", 365);
+  function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
-});
 
-darkModeToggleMobile.addEventListener("click", () => {
-  if (document.body.classList.contains("light")) {
-    activateDarkMode();
-    setCookie("theme", "dark", 365);
-  } else {
-    activateLightMode();
-    setCookie("theme", "light", 365);
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
   }
+
+  const lightModePreference = window.matchMedia("(prefers-color-scheme: light)");
+  lightModePreference.addEventListener("change", (e) => e.matches && activateLightMode());
+  lightModePreference.addListener((e) => e.matches && activateLightMode());
+  if (lightModePreference.matches) {
+    if (getCookie("theme") == "light") {
+      activateLightMode();
+    } else {
+      activateDarkMode();
+    }
+  } else {
+    if (getCookie("theme") == "dark") {
+      activateDarkMode();
+    } else {
+      activateLightMode();
+    }
+  }
+
+  darkModeToggle.addEventListener("click", () => {
+    if (document.body.classList.contains("light")) {
+      activateDarkMode();
+      setCookie("theme", "dark", 365);
+    } else {
+      activateLightMode();
+      setCookie("theme", "light", 365);
+    }
+  });
+
+  darkModeToggleMobile.addEventListener("click", () => {
+    if (document.body.classList.contains("light")) {
+      activateDarkMode();
+      setCookie("theme", "dark", 365);
+    } else {
+      activateLightMode();
+      setCookie("theme", "light", 365);
+    }
+  });
 });
